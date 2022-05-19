a094b8a7e6f120eb937c0edcda73c79c192a3e00
John-Pierre Clark <jpc@johnpierreclark.com>
2022-05-19 13:39:51 +0100

    data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { Container, Row } from "react-bootstrap";
+import { Container, Row} from "react-bootstrap";
 
 
 const IndexPage = () => (
